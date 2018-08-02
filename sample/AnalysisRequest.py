c7e47719ee1c9efc2fbf27bf4f10dbe316d1baa2
takeofuture <33204332+takeofuture@users.noreply.github.com>
2018-08-02 11:11:57 -0700

    Add files via upload

---
+++ sample/AnalysisRequest.py
@@ -0,0 +1,33 @@
+﻿#最初に動画の解析リクエストを送るサンプル
+import requests
+import json
+#送信先ＵＲＬ（笑顔、笑声解析サーバーのホスト）
+url = "http://192.168.17.94/Web015"
+headers = {
+    "Content-Type": "application/json"
+}
+
+#JSON内のＵＲＬはベルフェースの動画ダウンロードリンク（ＩＰによるフィルタを想定それ以外の認証は現在設定なし）
+body = {
+    "config":{
+        "analysys_sequence":10,
+        "climax_count_lower_limit":30,
+        "detect_count":2
+    },
+    "videos":[
+        {"url":"http://192.168.17.94/Bell001.mp4"},
+        {"url":"http://192.168.17.94/Bell002.mp4"},
+        {"url":"http://192.168.17.94/Bell003.mp4"},
+        {"url":"http://192.168.17.94/Bell004.mp4"},
+        {"url":"http://192.168.17.94/Bell005.mp4"},
+        {"url":"http://192.168.17.94/Bell006.mp4"},
+        {"url":"http://192.168.17.94/Bell007.mp4"},
+        {"url":"http://192.168.17.94/Bell008.mp4"},
+        {"url":"http://192.168.17.94/ext1.mp4"},
+        {"url":"http://192.168.17.94/ext2.mp4"},
+        {"url":"http://192.168.17.94/ext3.mp4"},
+    ]
+}
+r = requests.post(url, data=json.dumps(body), headers=headers)
+print(r.status_code)
+print(r.text)
---
+++ sample/JSONAcceptedResponse.py
@@ -0,0 +1,48 @@
+#POSTで送信された各動画の解析結果が表情分析システムからＰＯＳＴとしておくられてくるので
+#受領したらベルフェース側で以下のような{"status": "accepted"}を応答メッセージで返す
+#Apache、WSGIでのサンプル
+import sys
+import json
+import os
+from datetime import datetime
+#import requests
+import shutil
+from numpy.random import *
+import MySQLdb
+import time
+import ast
+import base64
+
+def application(environ,start_response):
+    status = '200 OK'
+    body = json.loads(environ['wsgi.input'].read())
+    if 'POST'!=environ.get('REQUEST_METHOD'):
+        outputstr = {"httperror": {"code": 1}}
+        outputjson = json.dumps(outputstr).encode('utf-8')
+        response_header = [('Content-type','application/json'),
+                        ('Content-Length',str(len(outputjson)))]						
+        start_response(status,response_header)
+        return [outputjson]
+   
+    jobcode="xxx"
+    if "requestjob" in body.keys():
+        requestjob = body["requestjob"]
+        jobcode = requestjob["jobcode"]
+    else:
+        outputstr = {"jsonerror": {"code": 1}}
+        outputjson = json.dumps(outputstr).encode('utf-8')
+        response_header = [('Content-type','application/json'),
+                        ('Content-Length',str(len(outputjson)))]						
+        start_response(status,response_header)
+        return [outputjson]
+    #JSONデータの保存サンプルは例として単純にファイルで保存する例
+    f = open("/tmp/"+jobcode+".json", "w")
+    json.dump(body, f)
+    f.close()
+　　　　#重要、ＪＳＯＮを受取確認できたら以下結果を送信する
+    outputstr = {"status": "accepted"}
+    outputjson = json.dumps(outputstr).encode('utf-8')
+    response_header = [('Content-type','application/json'),
+                    ('Content-Length',str(len(outputjson)))]						
+    start_response(status,response_header)	
+    return [outputjson]
